5a72c25342ac342512dc7536a9a0010c768ec95a
Vacker2005 <88232074+Vacker2005@users.noreply.github.com>
2023-09-20 17:42:20 +0600

    Add files via upload

---
+++ ConsoleApp9/ConsoleApp9/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace VectorLengthCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, y, z, length;
+
+            Console.WriteLine("Введите координату X:");
+            x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите координату Y:");
+            y = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите координату Z:");
+            z = Convert.ToDouble(Console.ReadLine());
+
+            length = Math.Sqrt(x * x + y * y + z * z); // Формула для вычисления длины вектора
+
+            Console.WriteLine("Длина вектора: " + length);
+
+            Console.ReadLine();
+        }
+    }
+}   
